6b33f5e726730296add1056799fbc79afd29d317
Werner Raath <werner123raath@gmail.com>
2017-10-18 13:09:13 +0200

    Doing socket connection instead of HTTP POST. Connection is still unsuccessful [Permission Denied].

--- README.md
+++ README.md
@@ -3,12 +3,16 @@
 **Solution Description**
 
 As the job listing is for Android development, I have written an Android app.
-The requirements were simple, post some data to a server and print the result.
+The requirements were simple, write some data to a TCP/IP server and print the result.
 I created a vanilla app with some basic UI components to visualise the change in app state.
 The page has two TextViews, one displaying the data to be posted and the other to display the response.
-To make the request I used the well known Volley library.
 When the request succeeds I show the response in the result TextView, otherwise I display the error.
+To make the request I used a Socket connection, attempting both the built-in java Socket module and also the Apache Commons Telnet module.
+Both methods threw a Permission denied error, so the connection could not be established and the xml could not be written to the server.
+
+The file of interest is: https://github.com/WernerRaath/BlueLabelApp/blob/master/app/src/main/java/com/example/wraath/myapplication/MainActivity.java
 
 **Response Received**
+None
 
 
--- app/src/main/java/com/example/wraath/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wraath/myapplication/MainActivity.java
@@ -1,34 +1,33 @@
 package com.example.wraath.myapplication;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.apache.commons.net.telnet.TelnetClient;
 
-// Using the Publicly available Volley library
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import java.io.UnsupportedEncodingException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    String url = "http://196.37.22.179:9011";
+    String ip = "196.37.22.179";
+    int port = 9011;
+    int attempt_count = 0;
 
     /*
         A convenience function to wrap key-value pairs in the correct XML format
      */
     private String wrapXmlTag(String tag, String value){
-        return "<" + tag + ">" + value + "</" + tag + ">";
+        return "<" + tag + ">" + value + "</" + tag + ">\n";
     }
 
     /*
@@ -36,15 +35,16 @@ public class MainActivity extends AppCompatActivity {
      */
     private String paramsToXML(String pin, String device_id, String device_ser, String device_ver,
                                String transType){
-        String xml = "<request><EventType>Authentication</EventType>";
-        xml += "<event>";
+        String xml = "<request>\n";
+        xml += this.wrapXmlTag("EventType", "Authentication");
+        xml += "<event>\n";
         xml += this.wrapXmlTag("UserPin", pin);
         xml += this.wrapXmlTag("DeviceId", device_id );
         xml += this.wrapXmlTag("DeviceSer", device_ser );
         xml += this.wrapXmlTag("DeviceVer", device_ver );
         xml += this.wrapXmlTag("TransType", transType );
-        xml += "</event>";
-        xml += "</request>";
+        xml += "</event>\n";
+        xml += "</request>\n";
         return xml;
     }
 
@@ -59,45 +59,52 @@ public class MainActivity extends AppCompatActivity {
 
         final String xml = this.paramsToXML(pin, device_id, device_ser, device_ver, trans_type);
         dataTextView.setText(xml);
-        // Create a request queue
-        RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        messageTextView.setText(response);
-                        messageTextView.setTextColor(Color.GREEN);
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        messageTextView.setText(error.getLocalizedMessage());
-                        messageTextView.setTextColor(Color.RED);
-                    }
-                }
-        ) {
-            @Override
-            public String getBodyContentType() {
-                return "application/xml; charset=" +
-                        getParamsEncoding();
-            }
-            @Override
-            public byte[] getBody() throws AuthFailureError {
-                try {
-                    return xml.getBytes(getParamsEncoding());
-                } catch (UnsupportedEncodingException uee) {
-                    messageTextView.setText(uee.getLocalizedMessage());
-                    messageTextView.setTextColor(Color.RED);
-                    return null;
-                }
-            }
-        };
+        String message = "Connecting to " + ip + ":" + port;
 
-        queue.add(request);
+        messageTextView.setText(message);
+        messageTextView.setTextColor(Color.BLUE);
+        attempt_count++;
 
+        try {
+            TelnetClient telnet = new TelnetClient();
+            telnet.connect(ip, port);
+            InputStream in = telnet.getInputStream();
+            PrintStream out = new PrintStream(telnet.getOutputStream());
+            out.println(xml);
+            out.flush();
+
+            String m, response = "";
+            while (true){
+                if ((m = in.read() + "") == null) break;
+                response += m;
+            }
+            in.close();
+            out.close();
+            telnet.disconnect();
+
+            Socket socket = new Socket(ip, port);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.print(xml + "\r\n");
+            writer.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            while (true){
+                if ((m = reader.readLine()) == null) break;
+                response += m + "\n";
+            }
+            writer.close();
+            reader.close();
+            socket.close();
+
+            message = response;
+            messageTextView.setTextColor(Color.GREEN);
+        } catch (Exception e){
+            message = e.getLocalizedMessage() +
+                    "\nConnection to " + ip + ":" + port + " failed...";
+            messageTextView.setTextColor(Color.RED);
+        }
+        messageTextView.setText("[Attempt " + attempt_count + "]: " + message );
     }
 
     @Override
